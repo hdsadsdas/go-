d6cd49687cb99bea74725bb299b9bc217f9342a7
曾凯 <84262595+hdsadsdas@users.noreply.github.com>
2022-03-11 11:00:43 +0800

    Add files via upload

---
+++ go语言区块链4/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"go语言区块链/go语言区块链4/BLC"
+)
+
+func main(){
+
+	// 创世区块
+	 blockchain := BLC.CreateBlockchainWithGenesisBlock()
+
+  	// 新区块
+     blockchain.AddBlockToBlockchain("sand 100RMB TO haha",blockchain.Blocks[len(blockchain.Blocks)-1].Height + 1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+
+	blockchain.AddBlockToBlockchain("sand 200RMB TO lala",blockchain.Blocks[len(blockchain.Blocks)-1].Height + 1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+
+	blockchain.AddBlockToBlockchain("sand 300RMB TO xixi",blockchain.Blocks[len(blockchain.Blocks)-1].Height + 1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+
+	blockchain.AddBlockToBlockchain("sand 50RMB TO hehe",blockchain.Blocks[len(blockchain.Blocks)-1].Height + 1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+
+
+	 fmt.Println(blockchain)
+	 fmt.Println(blockchain.Blocks)
+	 fmt.Println(blockchain.Blocks[2])
+
+}
