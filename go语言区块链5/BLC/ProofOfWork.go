e0a65c39926b5e8af4fb58e489548c300a6bf3de
曾凯 <84262595+hdsadsdas@users.noreply.github.com>
2022-03-12 08:50:38 +0800

    Add files via upload

---
+++ go语言区块链5/BLC/ProofOfWork.go
@@ -0,0 +1,22 @@
+package BLC
+
+type ProofOfWork struct {
+
+	Block *Block   //
+
+}
+
+func (ProofOfWork *ProofOfWork) Run()([]byte,int64){
+
+return nil,0
+
+}
+
+//创建新的工作量证明对象
+func NewProofOfWork(block *Block) *ProofOfWork  {
+
+	return &ProofOfWork{block}
+
+}
+
+
